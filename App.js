7f85120346ebbd2b56791b5a7869597a129864b6
wafuwafu13 <mariobaske@i.softbank.jp>
2020-03-15 21:58:05 +0900

    drawerをデザイン

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import Menu from './Menu';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Population from './Population/Population';
 import PopulationMenu from './Population/PopulationMenu';
 import EstimatePopulation from './Population/EstimatePopulation';
 import TransitionPopulation from './Population/TransitionPopulation';
+import Menu from './Menu';
+
 
 class App extends Component {
+
     render(){
-      const RootStack = createStackNavigator(
+      const Stack = createStackNavigator(
         {
           Home: Menu,
           Population: Population,
@@ -28,20 +32,25 @@ class App extends Component {
       const Drawer = createDrawerNavigator(
         {
           閉じる: {
-            screen: RootStack,
+            screen: Stack,
             navigationOptions:{
-              drawerIcon:  <Icon name="check" size={24} />
+              drawerIcon:  <Icon name="chevron-left" size={24} color="#636D70"/>
             }
           },
           グラフ選択: {
             screen: Menu,
             navigationOptions:{
-              drawerIcon:  <Icon name="check" size={24} />
+              drawerIcon:  <Icon name="chart-bar" size={24} color="#636D70"/>
             }
-          }
+          },
         },
         {
-          initialRouteName: '閉じる'
+          initialRouteName: '閉じる',
+          unmountInactiveRoutes: true,
+          drawerWidth: wp('25%'),
+          tintColor: 'red',
+          drawerBackgroundColor: '#F0FCFF'
+          
         }
       )
 
